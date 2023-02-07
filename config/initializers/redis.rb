362add72e3fe1b6565d11828e5a49b4243cf7933
tokiyaatsuya <tokiya.atsuya@gmail.com>
2023-02-08 00:15:35 +0900

    setup project

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 module RedisConn
   def self.current
-    @redis ||= Redis.new(url: ENV.fetch("REDIS_URL") { "redis://redis:6379/1" })
+    # @redis ||= Redis.new(url: ENV.fetch('REDIS_URL') { 'redis://redis:6379/1' })
+    @current ||= Redis.new(url: ENV.fetch('REDIS_URL') { 'redis://redis:6379/1' })
   end
 end
